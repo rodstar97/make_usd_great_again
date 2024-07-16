354333f39f2e7e004c54431cbe94fda9a6aae65b
Roderick Friedrich <r_friedrich@mailbox.org>
2024-07-16 10:22:51 +0200

    updates

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work/Projects/CPP/make_usd_great_again/scr/main.cpp" "CMakeFiles/test.dir/scr/main.cpp.o" "gcc" "CMakeFiles/test.dir/scr/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
